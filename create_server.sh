684ad5db81a4a13f5c26a557ddf6454c08617273
mustafa madiba <mustafamadiba2@gmail.com>
2023-08-21 13:33:52 +0100

    Update create_server.sh

--- create_server.sh
+++ create_server.sh
@@ -1,5 +1,5 @@
 aws cloudformation create-stack \
     --stack-name $1 \
-    --template-body file://final-project-starter.yml \
+    --template-body file://network.yml \
     --parameters file://server-parameters.json \
     --capabilities CAPABILITY_NAMED_IAM
